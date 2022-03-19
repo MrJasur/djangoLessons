dfe8c0c70575786dac3e3f0bfc5c930e7aa079df
MrJasur <jasurbekodilovn@gmail.com>
2022-03-19 12:44:37 +0900

    barcha templatelar uchun base.html yani qolib yaratib ularni sozladik

--- dars_3/pagesapp/urls.py
+++ dars_3/pagesapp/urls.py
@@ -5,6 +5,6 @@ from .views import HomePageView, AboutPageView, MainPageView, SingUpPageView
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'), #bu yerda, HomePageView(urls.py da korganimiz TemplateView dan voris olgandik) TemplateView ning .as_view() method dan foydalanyabdi
     path('about/', AboutPageView.as_view(), name='about'),
-    path('main/', MainPageView.as_view(), name='main'),
+    path('main/', MainPageView.as_view(), name='asosiy'),
     path('signup/', SingUpPageView.as_view(), name='signup'),
 ]
