752ffb933d14b3e7ff3d02b39fc2270204441ac4
MrJasur <jasurbekodilovn@gmail.com>
2022-03-19 12:19:33 +0900

    dars3 -bunda>> template papkasini yaratib uni ichiga 3 ta index fayl yaratib Url-View-Template larni sozladik

---
+++ dars_3/pagesapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView, MainPageView, SingUpPageView
+
+
+urlpatterns = [
+    path('home/', HomePageView.as_view(), name='home'), #bu yerda, HomePageView(urls.py da korganimiz TemplateView dan voris olgandik) TemplateView ning .as_view() method dan foydalanyabdi
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('main/', MainPageView.as_view(), name='main'),
+    path('signup/', SingUpPageView.as_view(), name='signup'),
+]
