a121cd620f6f7639571f69e7d35e59903428f458
MrJasur <jasurbekodilovn@gmail.com>
2022-03-20 16:32:13 +0900

    blog sayt oxiriga yetdi. SignUp, SignIn larni yaratdik

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogapp.urls'))
+    path('accounts/', include('django.contrib.auth.urls')), #biz hech qanday fayl yaratmadik. tayyorini chaqiryapmiz
+    path('accounts/', include('accounts.urls')),
+    path('', include('blogapp.urls')),
+
 ]
